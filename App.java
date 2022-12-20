9c6641e3f2661bd212bba2b2916a6af16d52da42
David <davidrojomar12@gamil.com>
2022-12-20 12:33:20 +0100

    modificacion app y readme

--- App.java
+++ App.java
@@ -4,6 +4,9 @@ public class App {
     System.out.println("Contenido 2a");
     System.out.println("Contenido 3");
     System.out.println("Contenido 2b");
+    System.out.println("David Rojo Martin");
+    System.out.println("20/12/22");
+    System.out.println("Ahora mismo vivo en León");
   }
   
 }
